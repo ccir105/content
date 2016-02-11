d951cd0f1c7cffc058db8b027340810e8aea3d9b
ccir105 <sisnet2010@gmail.com>
2016-02-11 16:50:57 +0545

    supplier country foreign removed

---
+++ database/migrations/2016_02_11_110441_remove_foreign_key.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropForeign('suppliers_country_id_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            //
+        });
+    }
+}
