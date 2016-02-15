010f1795dc2f9a1e9f624c31f02d85722278f2bf
ccir105 <sisnet2010@gmail.com>
2016-02-15 16:33:58 +0545

    Feed part finally done

--- modules/Supplier/Http/Controllers/FeedController.php
+++ modules/Supplier/Http/Controllers/FeedController.php
@@ -1,23 +1,88 @@
 <?php namespace Modules\Supplier\Http\Controllers;
 
 use Pingpong\Modules\Routing\Controller;
+use Symfony\Component\DomCrawler\Crawler;
 use Feeds;
 
 class FeedController extends Controller {
+
+	public $keywords = [
+		
+		'energy' => [
+			'Energie',
+			'Heizöl',
+			'Diesel',
+			'Holzpellets',
+		],
+		
+		'heat' => [
+			'Heizungen',
+			'Ölheizung',
+			'Wärmepumpe',
+			'Pelletsheizung',
+			'Gasheizung',
+			'Kamin'
+		],
+
+		'tank' => [
+			'Tankreinigung',
+			'Heizöl',
+			'Diesel',
+			'Holzpellets'
+		]
+	];
 	
 	public function getFeed()
 	{
-		$feed = Feeds::make('https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=Heizungen&output=rss');
+		$allNews = [];
 
-		$items = $feed->get_items();
+		foreach ( $this->keywords as $keywords ) {
+			$keyword =  $keywords[ array_rand( $keywords ) ];
+		
+			$feed = Feeds::make('https://news.google.com/news/section?q=' . $keyword. '&edchanged=1&ned=de_ch&authuser=0&output=rss');
+		
+			$items = $feed->get_items();
+		
+			$item = $items[ array_rand( $items ) ];
+		
+			$description = $item->get_description();
+	    
+	        $crawler = new Crawler($description);
+	    
+	        $firstTd = $crawler->filter('tr > td');
+	    
+	        $secondTd = $crawler->filter('tr > td + td');
+		
+			
+			$news = [
+				'title' => $item->get_title(),
+				'description' => '',
+				'link' => '',
+				'image' => ''
+			];
 
-		$return = [];
+			if( $firstTd->html() != "" ){
+				$news['link'] = $firstTd->filter('a')->attr('href');
+				$news['image'] = $firstTd->filter('img')->attr('src');		
+			}else{
+				$news['link'] = $secondTd->filter('a')->first()->attr('href');
+			}
 
-		foreach ( $items as $item ) {
-			$return[] = $item->get_description();
+
+			$descriptionHolder = $secondTd->filter('.lh');
+
+			$descriptionHolder->filter('a')->each(function($nodes){
+				foreach ($nodes as $node) {
+					$node->parentNode->removeChild($node);
+				}
+			});
+
+			$news['description'] = strip_tags($descriptionHolder->html());
+
+			$allNews[] = $news;
 		}
 
-		print_r($return);
+		return $allNews;
 	}
 	
 }
