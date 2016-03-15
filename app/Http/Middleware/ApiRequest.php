2ac38def208913d73f0de8654fce21ac249636be
ccir105 <sisnet2010@gmail.com>
2016-03-15 21:56:22 +0545

    configs

---
+++ app/Http/Middleware/ApiRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+use JWTAuth;
+use Closure;
+
+class ApiRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if( $user = JWTAuth::toUser() )
+        {
+            \Auth::loginUsingId($user->id);
+        }
+
+        return $next($request);
+    }
+}
---
+++ resources/views/modules/usermanagement/index.blade.php
@@ -0,0 +1,11 @@
+@extends('usermanagement::layouts.master')
+
+@section('content')
+	
+	<h1>Hello World</h1>
+	
+	<p>
+		This view is loaded from module: {!! config('usermanagement.name') !!}
+	</p>
+
+@stop
